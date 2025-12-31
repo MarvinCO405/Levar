4b5fc97eba38edc61a8e1932d3a2d4f85174e0f3
MarvinCO405 <marvincordova405@gmail.com>
2025-12-31 09:02:47 -0500

    Initial Commit

---
+++ LiftLogTests/LiftLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogTests.swift
+//  LiftLogTests
+//
+//  Created by Marvin Cordova Ortiz on 12/31/25.
+//
+
+import Testing
+@testable import LiftLog
+
+struct LiftLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
